1d69551078d47fe92269d74b5a741f6c6c73ec71
jibayimichang <74813784+jibayimichang@users.noreply.github.com>
2020-12-21 01:46:18 +0800

    Update hzm.js

--- Scripts/hzm.js
+++ Scripts/hzm.js
@@ -17,6 +17,7 @@ const headers = {
 'Accept' : `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`,
 'Connection' : `keep-alive`,
 'Host' : `api.turinglabs.net`,
+'Host' : `code.chiang.fun`,
 'User-Agent' : `Mozilla/5.0 (iPhone; CP iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.1 Mobile/15E148 Safari/604.1`,
 'Upgrade-Insecure-Requests' : `1`,
 'Accept-Language' : `zh-cn`
